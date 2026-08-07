802e41f8ef7ac922f274243c6ca48c0cf6276014
dharshan-m <dharshanmohan2704@gmail.com>
2026-08-07 21:22:55 +0530

    learned and developed the hash password and generate otp for signup

--- src/modules/auth/repositories/auth.repository.js
+++ src/modules/auth/repositories/auth.repository.js
@@ -2,4 +2,8 @@ const db = require('../../../config/db.js')
 
 const checkEmailExist = async(email)=>{
     const [rows] = await db.execute("SELECT * FROM users WHERE email = ?", [email]);
+    return rows.length > 0;
+}
+moduke.Exports={
+    checkEmailExist
 }
--- src/modules/auth/services/auth.service.js
+++ src/modules/auth/services/auth.service.js
@@ -1,6 +1,12 @@
 const authRepository = require('../repositories/auth.repository');
+const passwordUtil = require('../utils/password.util');
+const otpgeneration = require('../utils/otp.util');
 const signup=async(userData)=>{
     const emailExist = await authRepository.checkEmailExists(userData.email)
-
+    if(emailExist){
+        throw new Error("Email Already Exists");
+    }
+    const hashedPassword = await passwordUtil.hashPassword(userData.password);
+    const otp = otpgeneration.genereateOtp();
 }
 module.exports={signup}
